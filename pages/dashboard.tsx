9a8bfdaf35052ecf287ad65be5ff8205a76049cd
lahirutech <rmllbrathnayaka@gmail.com>
2023-05-17 15:18:45 +0530

    validate dashboard with auth

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,33 @@
+import { GetServerSideProps } from 'next';
+import { Session } from 'next-auth/core/types';
+import { getSession, signOut } from 'next-auth/react';
 import React from 'react';
 
-function Dashboard() {
-  return <div>My Dashboard </div>;
+export default function Dashboard({ session }: any) {
+  console.log('session', session);
+  return (
+    <div>
+      My Dashboard
+      <button onClick={() => signOut({ callbackUrl: '/login' })}>
+        Sign out
+      </button>
+    </div>
+  );
 }
 
-export default Dashboard;
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
